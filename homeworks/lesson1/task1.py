34434c40ce3aa0116f9945817e76d7d9ad7851ed
mnemonik01 <mnemonik_01@mail.ru>
2020-06-15 20:34:39 +0500

    hw lesson1

--- homeworks/lesson1/task1.py
+++ homeworks/lesson1/task1.py
@@ -1,1 +1,11 @@
-"homework"
+var_int_1 = 1
+var_int_2 = 2
+var_fl_1 = 3.01
+var_fl_2 = 4.002
+var_str_1 = 'abcd'
+var_str_2 = "efgh"
+
+print(var_int_1)
+https://github.com/mnemonik01/python_basic_11_06.gihttps://github.com/mnemonik01/python_basic_11_06.gitt
+
+
