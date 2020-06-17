494eb27565ae4f37b35073cc3229bb70814e59d3
mnemonik01 <mnemonik_01@mail.ru>
2020-06-18 01:50:37 +0500

    hw lesson1

--- homeworks/lesson1/task2.py
+++ homeworks/lesson1/task2.py
@@ -1,8 +1,8 @@
 sec_input = input("Введите время в секундах\n")
 if sec_input.isdigit():
     sec_input = int(sec_input)
-hours = sec_input//3600
-minutes = ((sec_input//60)%60)
-sec_output = (sec_input%60)
+    hours = sec_input//3600
+    minutes = ((sec_input//60)%60)
+    sec_output = (sec_input%60)
 
 print (hours,minutes,sec_output,sep=":")
---
+++ homeworks/lesson1/task5.py
@@ -0,0 +1,10 @@
+
+profit = int (input("Введите выручку:"))
+loss = int (input("Введите расходы:"))
+total = (profit-loss)
+if (total > 1) then
+    print ("Ваша прибыль составила:" total)
+
+
+
+#info = input ("Напишите несколько предложений о себе")
