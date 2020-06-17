c11fd93f1c7d051fb1410adce80ddf30608f7bad
mnemonik01 <mnemonik_01@mail.ru>
2020-06-17 18:16:23 +0500

    hw lesson1

--- homeworks/lesson1/task1.py
+++ homeworks/lesson1/task1.py
@@ -3,9 +3,15 @@ var_int_2 = 2
 var_fl_1 = 3.01
 var_fl_2 = 4.002
 var_str_1 = 'abcd'
-var_str_2 = "efgh"
+var_str_2 = "You are welcome!"
 
-print(var_int_1)
-https://github.com/mnemonik01/python_basic_11_06.gihttps://github.com/mnemonik01/python_basic_11_06.gitt
+print(var_str_2)
 
+name = input("Введите ваше имя")
+age = input ("Введите ваш возраст")
+info = input ("Напишите несколько предложений о себе")
+
+print(name)
+print(age)
+print(info)
 
