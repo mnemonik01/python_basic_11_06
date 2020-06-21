9122ada5329e7a64bc50fcd39c8868a8f7fd68e0
mnemonik01 <mnemonik_01@mail.ru>
2020-06-22 00:48:08 +0500

    hw lesson2

--- homeworks/lesson2/task3.py
+++ homeworks/lesson2/task3.py
@@ -2,3 +2,25 @@
 Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
 Напишите решения через list и через dict."""
 
+month = int (input("Введите номер месяца:"))
+seasons_list = ('зима',
+                'весна',
+                'лето',
+                'осень',
+                )
+
+seasons_dictionary = {'зима': (1, 2, 12),
+                'весна': (3, 4, 5),
+                'лето': (6, 7, 8),
+                'осень': (9, 10, 11),
+                }
+
+season_idx = month // 3 % 4
+
+for key, value in seasons_dictionary.items():
+    if month in value:
+        print(key)
+        break
+
+print(season_idx)
+print(seasons_list[season_idx])
--- homeworks/lesson2/task5.py
+++ homeworks/lesson2/task5.py
@@ -7,3 +7,25 @@
 Пользователь ввел число 1. Результат: 7, 5, 3, 3, 2, 1.
 Набор натуральных чисел можно задать непосредственно в коде, например, my_list = [7, 5, 3, 3, 2]."""
 
+my_list = [7, 5, 3, 3, 2]
+
+while True:
+    user_input = input('Введите новый элемент рейтинга:')
+    new_rating = abs(int(user_input))
+    new_rating_count = my_list.count(new_rating)
+
+    if not new_rating_count:
+        if new_rating > my_list[0]:
+            my_list.insert(0, new_rating)
+        elif new_rating < my_list[-1]:
+            my_list.append(new_rating)
+        else:
+            for idx, itm in enumerate(my_list):
+                if itm < new_rating:
+                    my_list.insert(idx, new_rating)
+                    break
+    else:
+        end_idx = my_list.index(new_rating) + new_rating_count
+        my_list.insert(end_idx, new_rating)
+
+    print(my_list)
