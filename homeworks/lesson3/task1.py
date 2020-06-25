4017d3f58ca902773777143ef1bb71a060369eb7
mnemonik01 <mnemonik_01@mail.ru>
2020-06-25 15:17:13 +0500

    hw lesson3

--- homeworks/lesson3/task1.py
+++ homeworks/lesson3/task1.py
@@ -7,8 +7,8 @@
 
 def div(*args):
     try:
-        arg1 = int(input("Введите делимое"))
-        arg2 = int(input("Введите делитель"))
+        arg1 = int(input("Введите  делимое"))
+        arg2 = int(input("Введите  делитель"))
         res = arg1 / arg2
     except ValueError:
         return 'Ошибка!'
